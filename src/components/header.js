5abd746724177c0d55c0aa3f50e935bd57b0237c
Rohan045 <1906045@kiit.ac.in>
2022-11-06 13:54:21 +0530

    first commit

---
+++ src/components/header.js
@@ -0,0 +1,24 @@
+import '../css/header.css';
+import {useState} from 'react';
+
+function Header(){
+    const [color,setColor] = useState(0);
+    const colors = ["white","green","red","purple","yellow"];
+    const changeColor = () => {
+        setColor((color + 1) % colors.length - 1);
+    }
+    return(
+        <>
+        <div className='header-body'>
+            <div className='logo' style={{color : `${colors[color]}`}}>
+                BlogSite.com
+            </div>
+            <div className='button'>
+                <h1><button onClick={changeColor}>Press</button></h1>
+            </div>
+        </div>
+        </>
+    );
+}
+
+export default Header;
