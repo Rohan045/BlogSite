dfb29e18d809cb1ccfaa1000c6eec2488e7fcac1
Rohan045 <1906045@kiit.ac.in>
2022-11-06 15:42:14 +0530

    navbar update

--- src/components/header.js
+++ src/components/header.js
@@ -1,22 +1,20 @@
 import '../css/header.css';
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 
 function Header(){
     const [color,setColor] = useState(0);
     const colors = ["white","green","red","purple","yellow"];
     const changeColor = () => {
-        setColor((color + 1) % colors.length - 1);
+        setColor((color + 1) % colors.length);
     }
     return(
         <>
         <div className='header-body'>
-            <div className='logo' style={{color : `${colors[color]}`}}>
+            <div className='logo' style={{color : `${colors[color]}`}} onClick={changeColor}>
                 BlogSite.com
             </div>
-            <p>Hello World!!</p>
-            <p>Hello World!! 2</p>
-            <div className='button'>
-                <h1><button onClick={changeColor}>Press</button></h1>
+            <div>
+                Entertainment | Wildlife | Photography | Coding
             </div>
         </div>
         </>
