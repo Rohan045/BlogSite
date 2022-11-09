bc8735af6d5c8da9568282d6fe862cbe90266f21
deepvashisth <deepvashisth7777@gmail.com>
2022-11-09 17:49:26 +0530

    deep version of navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import './App.css';
-import Header from './components/header';
-
+import { BrowserRouter , Switch, Route } from "react-router-dom";
+import Footer from './components/Footer';
+import HeaderTest from './components/HeaderTest';
+import Home from './components/Home';
+import CodingTab from './components/codingTab';
+import Entertainment from './components/Entertainment';
+import WildLife from './components/WildLife';
+import Photography from './components/Photography';
 function App() {
   return (
-    <div className='App'>
-        <Header/>
+    <div>
+      <BrowserRouter>
+        <HeaderTest />
+        <Switch>
+          <Route exact path="/" component={Home}></Route>
+          <Route path="/codingtab" component={CodingTab}></Route>
+          <Route path="/entertainment" component={Entertainment}></Route>
+          <Route path="/wildlife" component={WildLife}></Route>
+          <Route path="/photography" component={Photography}></Route>
+        </Switch>
+        {/* <Footer /> */}
+      </BrowserRouter>
     </div>
   );
 }
-
 export default App;
---
+++ src/components/Photography.js
@@ -0,0 +1,9 @@
+const Photography = () => {
+    return (
+      <div>
+        <p>Photography Material</p>
+      </div>
+    );
+  };
+  
+  export default Photography;
